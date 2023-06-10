36a86cee10e6ac3dcaf290d446729949d6b22d74
Carlos Rivera <cr4185320@gmail.com>
2023-06-10 01:52:00 -0500

    More path utils and handle multiple directory args.

--- code/main.cpp
+++ code/main.cpp
@@ -5,6 +5,8 @@
 #include <assert.h>
 #include <stdio.h>
 
+#include <vector>
+
 // NOTE: null terminated string with length
 // len does not include the null terminator
 // capacity does include the null terminator
@@ -32,7 +34,7 @@ struct Directory_Node {
     size_t child_num;
 };
 
-void path_str_grow(Path_Str *path, size_t num) {
+void path_grow(Path_Str *path, size_t num) {
     size_t old_cap = path->cap;
     size_t new_cap = 1;
     while (new_cap <= old_cap + num) {
@@ -43,127 +45,82 @@ void path_str_grow(Path_Str *path, size_t num) {
     path->cap = new_cap;
 }
 
-void path_str_ensure_cap(Path_Str *path, size_t len) {
+void path_ensure_cap(Path_Str *path, size_t len) {
     if (path->cap < path->len + 1 + len) {
-        path_str_grow(path, len);
+        path_grow(path, len);
     }
 }
 
-void path_str_append_char(Path_Str *path, char ch) {
-    path_str_ensure_cap(path, 1);
+void path_char(Path_Str *path, char ch) {
+    path_ensure_cap(path, 1);
     path->contents[path->len] = ch;
     path->contents[path->len+1] = '\0';
     path->len++;
 }
 
-void path_str_append(Path_Str *path, char *s) {
+void path_append(Path_Str *path, char *s) {
     size_t old_len = path->len;
     size_t str_len = strlen(s);
-    path_str_ensure_cap(path, str_len);
+    path_ensure_cap(path, str_len);
     memcpy(path->contents + old_len, s, str_len);
     path->contents[old_len + str_len] = '\0';
     path->len = old_len + str_len;
 }
 
-Path_Str path_str_copy(Path_Str path) {
+void path_prepend(Path_Str *path, char *s) {
+    size_t str_len = strlen(s);
+    size_t old_len = path->len;
+    path_ensure_cap(path, str_len);
+    char *old_contents = (char *)malloc(old_len);
+    memcpy(old_contents, path->contents, old_len);
+    memcpy(path->contents, s, str_len);
+    memcpy(path->contents + str_len, old_contents, old_len);
+    path->contents[old_len + str_len] = '\0';
+    path->len = old_len + str_len;
+}
+
+Path_Str path_copy(Path_Str path) {
     Path_Str result{};
     result.contents = (char *)malloc(path.cap);
     memcpy(result.contents, path.contents, path.cap);
+    result.contents[path.len] = '\0';
     result.len = path.len;
     result.cap = path.cap;
     return result;
 }
 
-Path_Str path_str_init(char *path, size_t len) {
+Path_Str path_init(char *path, size_t len) {
     Path_Str result{};
     result.contents = (char *)malloc(len + 1);
     memcpy(result.contents, path, len);
     result.contents[len] = '\0';
     result.len = len;
-    result.cap = len;
+    result.cap = len + 1;
     return result;
 }
 
-int concat_path(char *dest, size_t dest_size, char *src) {
-    size_t d = 0;
-    while (dest[d] != 0) {
-        d++;
-    }
+HANDLE hc;
+CONSOLE_SCREEN_BUFFER_INFO screen_buffer_info;
 
-    for (size_t s = 0; src[s] != 0; s++, d++) {
-        if (d >= dest_size) return -1;
-
-        dest[d] = src[s];
-    }
-
-    dest[d] = '\0';
-
-    return 0;
-}
-
-void replace_backslash(char **path) {
-    char *p = *path;
-    size_t i = 0;
-    while (p[i] != 0) {
-        if (p[i] == '\\') p[i] = '/';
-        i++;
-    }
-}
-
-int main(int argc, char **argv) {
-    HANDLE hc = GetStdHandle(STD_OUTPUT_HANDLE);
-    CONSOLE_SCREEN_BUFFER_INFO screen_buffer_info;
-    GetConsoleScreenBufferInfo(hc, &screen_buffer_info);
-
-    DWORD len = GetCurrentDirectory(0, nullptr);
-    char *current_path = (char *)malloc(len);
-    DWORD nret = GetCurrentDirectory(len, current_path);
-    assert(len == nret + 1);
-   
-    char *path = nullptr;
-    if (argc > 1) {
-        char *arg_path = argv[1];
-        path = (char *)calloc(strlen(arg_path) + 1, sizeof(char));
-
-        if (PathIsRelativeA(arg_path)) {
-            printf("Is reltiave\n");
-            size_t n = strlen(arg_path) + strlen(current_path) + 1;
-            path = (char *)realloc(path, n + 1);
-            strcpy(path, current_path);
-            concat_path(path, n, "\\");
-            concat_path(path, n, arg_path);
-        } else {
-            printf("Is absolute\n");
-            strcpy(path, arg_path);
-        }
-    } else {
-        path = (char *)calloc(strlen(current_path) + 1, sizeof(char));
-        strcpy(path, current_path);
-    }
-
-    char *find_path = (char *)calloc(strlen(path) + 2, sizeof(char));
-    strcpy(find_path, path);
-    concat_path(find_path, strlen(path) + 2, "\\*");
-
-    printf("Find Path: %s\n", find_path);
-
+void win32_print_directory_info(Path_Str directory_path) {
+    Path_Str find_path = path_copy(directory_path);
+    path_append(&find_path, "\\*");
+    printf("%s find:%s\n", directory_path.contents, find_path.contents);
     WIN32_FIND_DATAA find_data{};
-    HANDLE find_handle = FindFirstFileA(find_path, &find_data);
+    HANDLE find_handle = FindFirstFileA(find_path.contents, &find_data);
     assert(find_handle != INVALID_HANDLE_VALUE);
 
-    printf("%s:\n", path);
+
     do {
         int64_t file_size = 0;
         char *file_name = find_data.cFileName;
 
-        size_t path_len = strlen(path) + strlen(find_data.cFileName);
-        char *file_path = (char *)malloc(path_len + 2);
-        strcpy(file_path, path);
-        concat_path(file_path, path_len + 1, "\\");
-        concat_path(file_path, path_len + 1, find_data.cFileName);
-
+        Path_Str file_path = path_copy(directory_path);
+        path_append(&file_path, "\\");
+        path_append(&file_path, file_name);
+        
         DWORD file_attributes = find_data.dwFileAttributes;
-        HANDLE file_handle = CreateFileA(file_path, GENERIC_READ, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+        HANDLE file_handle = CreateFileA(file_path.contents, GENERIC_READ, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
         if (file_handle != INVALID_HANDLE_VALUE) {
             GetFileSizeEx(file_handle, (LARGE_INTEGER *)&file_size);
         }
@@ -173,15 +130,55 @@ int main(int argc, char **argv) {
         if (file_attributes & FILE_ATTRIBUTE_DIRECTORY) {
             SetConsoleTextAttribute(hc, FOREGROUND_BLUE);
         }
-        printf("%s path:%s\n", file_name, file_path);
+        printf("%s\n", file_name);
 
         SetConsoleTextAttribute(hc, 7);
 
         CloseHandle(file_handle);
-        free(file_path);
+        free(file_path.contents);
     } while (FindNextFileA(find_handle, &find_data));
 
-    FindClose(find_handle);
+    free(find_path.contents);
+}
+
+int main(int argc, char **argv) {
+    hc = GetStdHandle(STD_OUTPUT_HANDLE);
+    GetConsoleScreenBufferInfo(hc, &screen_buffer_info);
+
+    Path_Str current_path{};
+    {
+        DWORD len = GetCurrentDirectory(0, nullptr);
+        char *dir = (char *)malloc(len);
+        DWORD nret = GetCurrentDirectory(len, dir);
+        assert(len == nret + 1);
+        current_path = path_init(dir, len - 1);
+    }
+
+    std::vector<Path_Str> arg_paths;
+    for (int i = 1; i < argc; i++) {
+        char *arg = argv[i];
+        if (*arg == '-') continue;
+
+        Path_Str path = path_init(arg, strlen(arg));
+        arg_paths.push_back(path);
+    }
+
+    if (arg_paths.empty()) {
+        win32_print_directory_info(current_path);
+    }
+
+    for (int i = 0; i < arg_paths.size(); i++) {
+        Path_Str arg_path = arg_paths[i];
+        if (PathIsRelativeA(arg_path.contents)) {
+            Path_Str p = path_copy(current_path);
+            path_append(&p, "\\");
+            path_append(&p, arg_path.contents);
+            free(arg_path.contents);
+            arg_path = p;
+        }
+
+        win32_print_directory_info(arg_path);
+    }
 
     SetConsoleTextAttribute(hc, screen_buffer_info.wAttributes);
     return 0;
